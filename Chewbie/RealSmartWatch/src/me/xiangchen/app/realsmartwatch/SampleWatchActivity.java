7ad162976d9fe47c0a2d195f226daca0ac13809c
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2013-06-10 11:47:05 -0400

    added a smartwatch project deployable on the real watch

---
+++ Chewbie/RealSmartWatch/src/me/xiangchen/app/realsmartwatch/SampleWatchActivity.java
@@ -0,0 +1,13 @@
+package me.xiangchen.app.realsmartwatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleWatchActivity extends Activity{
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+	    finish();
+	  }
+}
